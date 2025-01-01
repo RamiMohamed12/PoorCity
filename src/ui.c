ec11945214fcdd02ae5e619886eb53772dc77512
RamiMohamed12 <roomierami00@gmail.com>
2025-01-01 14:18:52 +0000

    updated ui.c

--- src/ui.c
+++ src/ui.c
@@ -1,34 +1,28 @@
 #include "ui.h"
-#include <SDL2/SDL.h>
-#include <stdio.h>
-
-void handle_input(int key, Population *pop, Resources *resources, Buildings *buildings) {
-    switch (key) {
-        case SDLK_1: { // Assign population to gather food
-            printf("Assign how many people to gather food: ");
-            int num;
-            scanf("%d", &num);
-            assign_population(pop, "food", num);
-            break;
-        }
-        case SDLK_2: { // Construct a building
-            printf("Enter building type (house, castle, shop, marketplace): ");
-            char building_type[20];
-            scanf("%s", building_type);
-            construct_building(buildings, resources, building_type);
-            break;
-        }
-        default:
-            printf("Invalid input.\n");
-    }
-}
+#include "render_text.h"
 
 void render_ui(SDL_Renderer *renderer, Population *pop, Resources *resources, Buildings *buildings) {
-    // Placeholder: Render resource and population stats
-    printf("Population: %d (Unassigned: %d)\n", pop->total_population, pop->unassigned);
-    printf("Resources - Food: %d, Wood: %d, Stone: %d, Concrete: %d\n",
-           resources->food, resources->wood, resources->stone, resources->concrete);
-    printf("Buildings - Houses: %d, Castles: %d, Shops: %d, Marketplaces: %d\n",
-           buildings->houses, buildings->castles, buildings->shops, buildings->marketplaces);
+    char buffer[128];
+    SDL_Color white = {255, 255, 255, 255};
+
+    // Render population
+    snprintf(buffer, sizeof(buffer), "Population: %d (Unassigned: %d)", pop->total_population, pop->unassigned);
+    render_text(renderer, buffer, 20, 20, white);
+
+    // Render resources
+    snprintf(buffer, sizeof(buffer), "Food: %d | Wood: %d | Stone: %d | Concrete: %d",
+             resources->food, resources->wood, resources->stone, resources->concrete);
+    render_text(renderer, buffer, 20, 50, white);
+
+    // Render buildings
+    snprintf(buffer, sizeof(buffer), "Houses: %d | Castles: %d | Shops: %d | Marketplaces: %d",
+             buildings->houses, buildings->castles, buildings->shops, buildings->marketplaces);
+    render_text(renderer, buffer, 20, 80, white);
+}
+
+void animate_resource_gathering(SDL_Renderer *renderer, int x, int y, const char *resource_type) {
+    SDL_Color green = {0, 255, 0, 255};
+    render_text(renderer, "+2", x, y, green); // Example: Floating "+2" effect
+    SDL_Delay(200); // Temporary animation effect
 }
 
